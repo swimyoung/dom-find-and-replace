488411df9977eb81966fd6bcb211f23254105977
swimyoung <swimyoung@gmail.com>
2021-04-03 23:32:22 +0900

    Update dependencies and remove webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint'],
   extends: [
+    'eslint:recommended',
+    'plugin:jest/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
   ],
 };
