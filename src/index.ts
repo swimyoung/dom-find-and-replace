8be18f51d54889de9eab3e52bbe00adf9ac5097b
swimyoung <swimyoung@gmail.com>
2021-04-17 19:17:25 +0900

    Remove useless iife

--- src/index.ts
+++ src/index.ts
@@ -3,72 +3,70 @@ import randomColor from 'randomcolor';
 import { makeRandomHTML } from './randomContent';
 import { findAndReplace, Recover } from 'dom-find-and-replace';
 
-(() => {
-  const state = {
-    find: 'a',
-    replace: '',
-    content: makeRandomHTML(),
-  };
-  const elements = {
-    find: document.getElementById('query-find') as HTMLInputElement,
-    replace: document.getElementById('query-replace') as HTMLInputElement,
-    content: document.getElementById('content') as HTMLDivElement,
-  };
+const state = {
+  find: 'a',
+  replace: '',
+  content: makeRandomHTML(),
+};
+const elements = {
+  find: document.getElementById('query-find') as HTMLInputElement,
+  replace: document.getElementById('query-replace') as HTMLInputElement,
+  content: document.getElementById('content') as HTMLDivElement,
+};
 
-  const find = ((recover: Recover) => (value: string) => {
-    if (!value) {
-      elements.content.innerHTML = state.content;
-      return;
-    }
+const find = ((recover: Recover) => (value: string) => {
+  if (!value) {
+    elements.content.innerHTML = state.content;
+    return;
+  }
 
-    recover();
+  recover();
 
-    let previousFoundText: string;
-    let color = randomColor();
-    // find text and add highlight
-    recover = findAndReplace(elements.content, {
-      find: value,
-      replace: (offsetText, foundText) => {
-        color = previousFoundText !== foundText ? randomColor() : color;
-        previousFoundText = foundText;
+  let previousFoundText: string;
+  let color = randomColor();
+  // find text and add highlight
+  recover = findAndReplace(elements.content, {
+    find: value,
+    replace: (offsetText, foundText) => {
+      color = previousFoundText !== foundText ? randomColor() : color;
+      previousFoundText = foundText;
 
-        const span = document.createElement('span');
-        span.textContent = offsetText;
-        span.setAttribute('style', 'background-color: ' + color + ';');
-        return span;
-      },
-    }) as Recover;
+      const span = document.createElement('span');
+      span.textContent = offsetText;
+      span.setAttribute('style', 'background-color: ' + color + ';');
+      return span;
+    },
+  }) as Recover;
 
-    state.replace = '';
-  })(() => undefined);
+  state.replace = '';
+})(() => undefined);
 
-  const replace = ((recover: Recover) => (value: string) => {
-    if (!value) {
-      elements.content.innerHTML = state.content;
-      find(state.find);
-      return;
-    }
+const replace = ((recover: Recover) => (value: string) => {
+  if (!value) {
+    elements.content.innerHTML = state.content;
+    find(state.find);
+    return;
+  }
 
-    recover();
+  recover();
 
-    // replace text
-    recover = findAndReplace(elements.content, {
-      find: state.find,
-      replace: value,
-    }) as Recover;
-  })(() => undefined);
+  // replace text
+  recover = findAndReplace(elements.content, {
+    find: state.find,
+    replace: value,
+  }) as Recover;
+})(() => undefined);
 
-  elements.find?.addEventListener('input', (event) => {
-    find((event.target as HTMLInputElement).value);
-  });
-  elements.replace?.addEventListener('input', (event) => {
-    replace((event.target as HTMLInputElement).value);
-  });
-  elements.content?.addEventListener('input', () => {
-    state.content = elements.content.innerHTML;
-  });
+elements.find?.addEventListener('input', (event) => {
+  find((event.target as HTMLInputElement).value);
+});
+elements.replace?.addEventListener('input', (event) => {
+  replace((event.target as HTMLInputElement).value);
+});
+elements.content?.addEventListener('input', () => {
+  state.content = elements.content.innerHTML;
+});
 
-  elements.content.innerHTML = state.content;
-  elements.find.value = state.find;
-  find(state.find);
-})();
+elements.content.innerHTML = state.content;
+elements.find.value = state.find;
+find(state.find);
